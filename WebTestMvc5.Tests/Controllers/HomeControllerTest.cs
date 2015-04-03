e7d028d76888790189ac8662940cead1f3bb09b2
Yuichi Ikeda <ikeday@kita-osaka.co.jp>
2015-04-03 14:58:33 +0900

    first commit

---
+++ WebTestMvc5.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebTestMvc5;
+using WebTestMvc5.Controllers;
+
+namespace WebTestMvc5.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // 準備
+            HomeController controller = new HomeController();
+
+            // 実行
+            ViewResult result = controller.Index() as ViewResult;
+
+            // アサート
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ WebTestMvc5/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebTestMvc5.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
